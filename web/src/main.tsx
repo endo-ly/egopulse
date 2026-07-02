892031dfb2fa67bfa30ed43d91db73d7913255d4
ryuto.endo <ryuto9007@gmail.com>
2026-07-02 01:33:19 +0000

    feat(web): define design tokens in app.css

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,7 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./components/App";
 import "./app.css";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const root = document.getElementById("root");
+if (root) {
+  createRoot(root).render(null);
+}
